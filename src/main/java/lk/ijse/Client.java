0a2caa7ec49dfd12b121b06fa1c02c9af437394a
chathunga2007 <wggachathungabimsara2007@gmail.com>
2026-04-29 11:51:41 +0530

    initial commit

--- src/main/java/lk/ijse/Client.java
+++ src/main/java/lk/ijse/Client.java
@@ -1,17 +1,14 @@
 package lk.ijse;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try {
+            Socket remoteSocket = new Socket("127.0.0.1", 3000);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/main/java/lk/ijse/Server.java
+++ src/main/java/lk/ijse/Server.java
@@ -1,4 +1,19 @@
 package lk.ijse;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(3000);
+            System.out.println("Server Started!");
+
+            Socket socket = serverSocket.accept();
+            System.out.println("Client Connected!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
