c710a82dea1a5f03af8853128d0f2ddea2407298
chathunga2007 <wggachathungabimsara2007@gmail.com>
2026-04-29 11:58:35 +0530

    initial commit

--- src/main/java/lk/ijse/Client.java
+++ src/main/java/lk/ijse/Client.java
@@ -1,5 +1,6 @@
 package lk.ijse;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
@@ -7,6 +8,10 @@ public class Client {
     public static void main(String[] args) {
         try {
             Socket remoteSocket = new Socket("127.0.0.1", 3000);
+            DataOutputStream dos = new DataOutputStream(remoteSocket.getOutputStream());
+            dos.writeUTF("Hello Server!");
+            dos.flush();
+            remoteSocket.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
